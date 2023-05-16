a06c0f4eb3eb8ddad8743fdf28ade8a768ddc4e1
abdyraman <abdyramanova.j@gmail.com>
2023-05-16 19:32:20 -0400

    table schema

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE category (
+    category_id VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(255) NOT NULL,
+    subcategory VARCHAR(255) NOT NULL,
+    PRIMARY KEY (subcategory_id)
+);
+
+CREATE TABLE contacts (
+    contact_id INTEGER NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (contact_id)
+);
+
+CREATE TABLE campaign (
+    cf_id INTEGER NOT NULL,
+    contact_id INTEGER NOT NULL,
+    company_name VARCHAR(255) NOT NULL,
+    descriptions VARCHAR(255) NOT NULL,
+    goal FLOAT NOT NULL,
+    pledged FLOAT NOT NULL,
+    outcome VARCHAR(255) NOT NULL,
+    backers_count INTEGER NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    currency VARCHAR(255) NOT NULL,
+    launch_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR(255) NOT NULL,
+    subcategory_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
+    FOREIGN KEY (category_id) REFERENCES category (category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id),
+    PRIMARY KEY (cf_id)
+);
+
+
+   
